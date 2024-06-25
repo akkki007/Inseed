14421fcdfdb423f633a1a2feef7f359bd53fd8d4
Rohan <rohanwani10032006@gmail.com>
2024-06-25 16:12:38 +0530

    Login complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from './components/Home'
 import Login from './components/Login'
 import Placements from './components/Placements'
 import Registration from './components/Registration'
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
